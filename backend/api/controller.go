bbdcd9119db011fd02ec259e42602150c61e5152
soartec-lab <info@soartec-lab.work>
2024-03-14 09:53:18 +0900

    add sample controller

--- backend/api/controller.go
+++ backend/api/controller.go
@@ -11,7 +11,25 @@ func NewController() *Controller {
 }
 
 func (c *Controller) ListPets(ctx context.Context, request ListPetsRequestObject) (ListPetsResponseObject, error) {
-	return nil, nil
+	pets := Pets{
+		{
+			Id:   1,
+			Name: "Fido",
+			Tag:  nil,
+		},
+		{
+			Id:   2,
+			Name: "Fifi",
+			Tag:  nil,
+		},
+	}
+
+	res := ListPets200JSONResponse{
+		Body: pets,
+		Headers: ListPets200ResponseHeaders{},
+	}
+
+	return res, nil
 }
 
 func (c *Controller) CreatePets(ctx context.Context, request CreatePetsRequestObject) (CreatePetsResponseObject, error) {
