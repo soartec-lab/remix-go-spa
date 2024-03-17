f5d76104ea545bc21b084dd2407c5c4836d0b14a
soartec-lab <info@soartec-lab.work>
2024-03-17 13:13:06 +0900

    update to find data using database

--- backend/api/controller.go
+++ backend/api/controller.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"context"
+	"gorm.io/gorm"
+
+	"github.com/soartec-lab/remix-go-spa/model"
 )
 
 type Controller struct {}
@@ -11,21 +14,26 @@ func NewController() *Controller {
 }
 
 func (c *Controller) ListPets(ctx context.Context, request ListPetsRequestObject) (ListPetsResponseObject, error) {
-	pets := Pets{
-		{
-			Id:   1,
-			Name: "Fido",
-			Tag:  nil,
-		},
-		{
-			Id:   2,
-			Name: "Fifi",
-			Tag:  nil,
-		},
+	db := ctx.Value("db")
+	gormDB := db.(*gorm.DB)
+
+	var pets []model.Pet
+	result := gormDB.Find(&pets)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	var responsePets Pets
+	for _, pet := range pets {
+		responsePets = append(responsePets, Pet{
+			Id:   int64(pet.ID),
+			Name: pet.Name,
+			Tag:  &pet.Tag,
+		})
 	}
 
 	res := ListPets200JSONResponse{
-		Body: pets,
+		Body: responsePets,
 		Headers: ListPets200ResponseHeaders{},
 	}
 
--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+  "gorm.io/driver/mysql"
 
 	"github.com/soartec-lab/remix-go-spa/api"
 )
@@ -16,6 +18,17 @@ func main() {
 	flag.Parse()
 
 	r := gin.Default()
+
+	dsn := "root:password@tcp(mysql:3306)/main?charset=utf8mb4&parseTime=True&loc=Local"
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
 
 	controller := api.NewController()
 	strictHandler := api.NewStrictHandler(controller, nil)
