a80f52f2f06f262027352ee6e77e546dd90cc390
soartec-lab <info@soartec-lab.work>
2024-04-30 23:48:21 +0000

    using `clientLoader`

--- frontend/apps/remix-spa/app/routes/pets_.$id/index.tsx
+++ frontend/apps/remix-spa/app/routes/pets_.$id/index.tsx
@@ -1,10 +1,18 @@
-import { useParams } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
+
+import type { Pet as PetType } from "~/repositories/client/models";
+import { showPetById } from "~/repositories/client/pets/pets";
 
 import Pet from "./pet";
 
-export default function PetPage() {
-	const params = useParams();
-	const id = params.id;
+export async function clientLoader({ params }: { params: { id: string } }) {
+	const pet = await showPetById(params.id, { page: "1" });
 
-	return <Pet id={id} />;
+	return pet.data as PetType;
+}
+
+export default function PetPage() {
+	const pet = useLoaderData<typeof clientLoader>();
+
+	return <Pet pet={pet} />;
 }
--- frontend/apps/remix-spa/app/routes/pets_.$id/pet.tsx
+++ frontend/apps/remix-spa/app/routes/pets_.$id/pet.tsx
@@ -1,15 +1,16 @@
 import { Button } from "~/components/ui/button";
+import type { Pet as PetType } from "~/repositories/client/models";
 
 type Props = {
-	id: string;
+	pet: PetType;
 };
 
-export default function Pet({ id }: Props) {
+export default function Pet({ pet }: Props) {
 	return (
 		<div>
 			<Button onClick={() => console.log("hellow")}>Click me</Button>
 			<h1>Pet</h1>
-			<p>Pet ID: {id}</p>
+			<p>Pet ID: {pet.id}</p>
 		</div>
 	);
 }
