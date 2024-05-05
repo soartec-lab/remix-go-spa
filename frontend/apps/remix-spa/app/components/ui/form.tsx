ed7ecdf85deb63f1a5dc3c3860003f7e7ae9e947
soartec-lab <info@soartec-lab.work>
2024-05-05 11:22:33 +0000

    move `app/util.ts` to `components/ui/util.ts`

--- frontend/apps/remix-spa/app/components/ui/form.tsx
+++ frontend/apps/remix-spa/app/components/ui/form.tsx
@@ -11,7 +11,7 @@ import {
 } from "react-hook-form";
 
 import { Label } from "~/components/ui/label";
-import { cn } from "~/utils";
+import { cn } from "~/components/ui/utils";
 
 const Form = FormProvider;
 
--- frontend/apps/remix-spa/app/components/ui/toast.tsx
+++ frontend/apps/remix-spa/app/components/ui/toast.tsx
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { X } from "lucide-react";
 import * as React from "react";
 
-import { cn } from "~/utils";
+import { cn } from "~/components/ui/utils";
 
 const ToastProvider = ToastPrimitives.Provider;
 
